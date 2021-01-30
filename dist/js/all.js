dc31fe7ac84ebdebb29ad0ed5e71f727500f3ecc
Chris Chen <chiandeye@gmail.com>
2021-01-30 18:48:45 +0800

    Add html, css, swiper

--- dist/js/all.js
+++ dist/js/all.js
@@ -100,20 +100,90 @@ $(document).ready(function () {
 			e = $(document).scrollTop()
 		})
 	});
-
+	//js slider
+	window.onload = function() {
+		$('.slider1').jdSlider();
+		$('.slider2').jdSlider({
+				wrap: '.slide-inner',
+				isAuto: true,
+				isLoop: true
+		});
+		$('.slider3').jdSlider({
+				wrap: '.slide-inner',
+				slideShow: 2,
+				slideToScroll: 2,
+				isLoop: false,
+				responsive: [{
+						viewSize: 768,
+						settings: {
+								slideShow: 1,
+								slideToScroll: 1
+						}
+				}]
+		});
+		$('.slider3-2').jdSlider({
+				wrap: '.slide-inner',
+				slideShow: 2,
+				slideToScroll: 1,
+				isLoop: true,
+				responsive: [{
+						viewSize: 768,
+						settings: {
+								slideShow: 1
+						}
+				}]
+		});
+		$('.slider3-3').jdSlider({
+				wrap: '.slide-inner',
+				slideShow: 2,
+				slideToScroll: 2,
+				isLoop: true,
+				responsive: [{
+						viewSize: 768,
+						settings: {
+								slideShow: 1,
+								slideToScroll: 1
+						}
+				}]
+		});
+		$('.slider4').jdSlider({
+				wrap: '.slide-inner',
+				isSliding: false,
+				isAuto: true,
+				isLoop: true,
+				isDrag: false
+		});
+};
+	//
 	//swiper
+	// var swiper = new Swiper('.swiper-container', {
+	// 	speed: 600,
+	// 	parallax: true,
+	// 	loop: true,
+	// 	pagination: {
+	// 		el: '.swiper-pagination',
+	// 		clickable: true,
+	// 	},
+	// 	navigation: {
+	// 		nextEl: '.swiper-button-next',
+	// 		prevEl: '.swiper-button-prev',
+	// 	},
+	// });
+	//
 	var swiper = new Swiper('.swiper-container', {
-		speed: 600,
-		parallax: true,
-		loop: true,
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
+		effect: 'coverflow',
+		grabCursor: true,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		coverflowEffect: {
+				rotate: 50,
+				stretch: 0,
+				depth: 100,
+				modifier: 1,
+				slideShadows: true,
 		},
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
+		pagination: {
+				el: '.swiper-pagination',
 		},
 	});
-	//
 });
