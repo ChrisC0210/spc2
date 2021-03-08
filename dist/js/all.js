f56ef6e3c31446687c14929fe2adb955c3f8cc6d
Chris Chen <chiandeye@gmail.com>
2021-03-09 02:37:46 +0800

    Add html, css, video

--- dist/js/all.js
+++ dist/js/all.js
@@ -233,9 +233,9 @@ var swiper = new Swiper('.swiper-container-2', {
 	loop: true,
 	// autoplay: {
 	// 	delay: 5000,
-	// 	true
 	// },
 	autoplay: true,
+	spaceBetween: 30,
 	effect: 'fade',
 	freeMode: true,
   pagination: {
@@ -276,4 +276,24 @@ swiper.on('slideChange', function () {
   currentVideo.trigger('play');
 });
 		//
+		//
+		//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}		
 });
